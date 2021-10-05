434c2c1da43d0996bc6c59a7ba48c4fd745d8e5b
SMAkbar <sm.akbar42@gmail.com>
2021-10-05 18:41:36 +0500

    All required functionality added

--- app/src/main/java/com/akbar/tiptime/MainActivity.kt
+++ app/src/main/java/com/akbar/tiptime/MainActivity.kt
@@ -2,10 +2,41 @@ package com.akbar.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.akbar.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener {  calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
+
+        // taking value from radio buttons
+        val percent = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.2
+            R.id.option_eighteen_percent -> 0.18
+            R.id.option_fifteen_percent -> 0.15
+            else -> 0.0
+        }
+        var tip = cost * percent
+
+
+        if (binding.roundUpSwitch.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+
+        binding.tipAmount.text = getString(R.string.total_tip, formattedTip)
     }
 }
